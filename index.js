64ace55f9cb1b5e77f145023fc59e38f10babfee
vivian waithera <vivyannekori@gmail.com>
2023-06-13 20:39:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance = 199
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance >= 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? ('Ok, sounds good.') : ("No go."))
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+// const tip = 'generous';
+
+// let cheers;
+
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+    case 'generous':
+     return 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+  
+     return 'Thank you.';
+     break;
+
+    default:
+      return 'Bye.'
+  }
 }
